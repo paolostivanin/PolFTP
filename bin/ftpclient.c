30ca5640e1cde242c262dc8bb8fa45f1b3cb646a
Paolo Stivanin <admin@polslinux.it>
2012-04-19 16:53:41 +0200

    hide pwd feature

--- bin/ftpclient.c
+++ bin/ftpclient.c
@@ -1,28 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h> 
 
 int main(){
+
 char *host, *user, *pass;
+
 host = malloc(64 * sizeof(char)); /* space for 64 chars */
 if(host == NULL){
 	printf("\n--> ERRORE: memoria non allocata\n");
 	return EXIT_FAILURE;
 }
+
 user = malloc(64 * sizeof(char)); /* space for 64 chars */
 if(user == NULL){
 	printf("\n--> ERRORE: memoria non allocata\n");
 	return EXIT_FAILURE;
 }
+
 pass = malloc(64 * sizeof(char)); /* space for 64 chars */
 if(pass == NULL){
 	printf("\n--> ERRORE: memoria non allocata\n");
 	return EXIT_FAILURE;
 }
-puts("Inserisci <host user pwd>:");
-scanf("%s %s %s", host,user,pass);
-printf("Host: %s\nUser: %s\nPWD: %s\n", host,user,pass);
+
+puts("--> Inserisci <hostname username>:");
+scanf("%s %s", host,user);
+pass = getpass("--> Inserisci password: ");
+
+printf("Host: %s\nUser: %s\nPass: %s\n", host,user,pass);
+
 free(host);
 free(user);
 free(pass);
+
 return EXIT_SUCCESS;
 }
