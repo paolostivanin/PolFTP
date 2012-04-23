503ae6c99a3c5e8633d5ae6286d124c0251f526b
Paolo Stivanin <admin@polslinux.it>
2012-04-23 16:05:53 +0200

    small changes

--- bin/ftpclient.c
+++ bin/ftpclient.c
@@ -11,36 +11,38 @@
 #include <unistd.h> 
 
 int main(){
+	char *host, *user, *pass;
 
-char *host, *user, *pass;
+	host = malloc(64 * sizeof(char)); /* spazio per 64 caratteri */
+	if(host == NULL){
+		printf("\n--> ERRORE: memoria non allocata\n");
+		return EXIT_FAILURE;
+	}
 
-host = malloc(64 * sizeof(char)); /* space for 64 chars */
-if(host == NULL){
-	printf("\n--> ERRORE: memoria non allocata\n");
-	return EXIT_FAILURE;
-}
+	user = malloc(64 * sizeof(char)); /* spazio per 64 caratteri */
+	if(user == NULL){
+		printf("\n--> ERRORE: memoria non allocata\n");
+		return EXIT_FAILURE;
+	}
 
-user = malloc(64 * sizeof(char)); /* space for 64 chars */
-if(user == NULL){
-	printf("\n--> ERRORE: memoria non allocata\n");
-	return EXIT_FAILURE;
-}
+	pass = malloc(64 * sizeof(char)); /* spazio per 64 caratteri */
+	if(pass == NULL){
+		printf("\n--> ERRORE: memoria non allocata\n");
+		return EXIT_FAILURE;
+	}
 
-pass = malloc(64 * sizeof(char)); /* space for 64 chars */
-if(pass == NULL){
-	printf("\n--> ERRORE: memoria non allocata\n");
-	return EXIT_FAILURE;
-}
+	/* Immissione di hostname, username e password */
+	puts("--> Inserisci <hostname username>:");
+	scanf("%s %s", host,user);
+	pass = getpass("--> Inserisci password: ");
+	
+	/* Stampo a video le informazioni immesse */
+	printf("Host: %s\nUser: %s\nPass: %s\n", host,user,pass);
 
-puts("--> Inserisci <hostname username>:");
-scanf("%s %s", host,user);
-pass = getpass("--> Inserisci password: ");
+	/* Libero la memoria occupata */
+	free(host);
+	free(user);
+	free(pass);
 
-printf("Host: %s\nUser: %s\nPass: %s\n", host,user,pass);
-
-free(host);
-free(user);
-free(pass);
-
-return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
