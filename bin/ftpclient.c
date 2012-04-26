d08c213268d64620e6ba25af1670164c5bcbad24
Paolo Stivanin <admin@polslinux.it>
2012-04-26 15:27:12 +0200

    minor fix

--- bin/ftpclient.c
+++ bin/ftpclient.c
@@ -13,11 +13,12 @@
 #include <termios.h>
 
 int main(){
+	
 	char *host, *user, *pass;
 
 	host = malloc(64); /* spazio per max 64 caratteri */
 	if(!host) abort(); /* se malloc ritorna NULL allora termino l'esecuzione */
-	host[63] = '\0'; /* evitare un tipo di buffer overflow impostando l'ultimo byte come NUL byte */
+	host[63] = '\0';   /* evitare un tipo di buffer overflow impostando l'ultimo byte come NUL byte */
 
 	user = malloc(64);
 	if(!user) abort();
@@ -28,41 +29,44 @@ int main(){
 	pass[63] = '\0';
 
 	/* Immissione di hostname, username e password */
-	fprintf(stdout,"--> Inserisci <hostname username>: ");
-	fscanf(stdin, "%s %s", host, user);
+	fprintf(stdout,"--> Inserisci <hostname>: ");
+	fscanf(stdin, "%63s", host);
+	fprintf(stdout,"\n--> Inserisci <username>: ");
+	fscanf(stdin, "%63s", user);
 	fprintf(stdout, "\n--> Inserisci <password>: ");
 	
 	/* Grazie a termios.h posso disabilitare l'echoing del terminale (password nascosta) */
 	struct termios term, term_orig;
-    tcgetattr(STDIN_FILENO, &term);
-    term_orig = term;
-    term.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-    /* -- */
-    fscanf(stdin, "%s", pass);
+   	tcgetattr(STDIN_FILENO, &term);
+    	term_orig = term;
+    	term.c_lflag &= ~ECHO;
+    	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+    	/* -- */
+    	fscanf(stdin, "%63s", pass);
     
-    /* Reimposto il terminale allo stato originale (altrimenti echoing resta disabilitato */
-    tcsetattr(STDIN_FILENO, TCSANOW, &term_orig);
+    	/* Reimposto il terminale allo stato originale (altrimenti l'echoing resta disabilitato) */
+    	tcsetattr(STDIN_FILENO, TCSANOW, &term_orig);
 	
 	if((strlen(host)+1) > 64){ /* controllo la lunghezza della stringa inserita (+1 per carattere terminazione \0) */
-		fprintf(stderr,"\n--> ERRORE: La stringa deve essere minore di 64 caratteri.\n");
+		fprintf(stderr,"\n--> ERRORE: L'hostname deve essere minore di 64 caratteri.\n");
 		return EXIT_FAILURE;
 	}
 	
 	if((strlen(user)+1) > 64){
-		fprintf(stderr,"\n--> ERRORE: La stringa deve essere minore di 64 caratteri.\n");
+		fprintf(stderr,"\n--> ERRORE: Lo username deve essere minore di 64 caratteri.\n");
 		return EXIT_FAILURE;
 	}
 	
 	if((strlen(pass)+1) > 64){
-		fprintf(stderr,"\n--> ERRORE: La stringa deve essere minore di 64 caratteri.\n");
+		fprintf(stderr,"\n--> ERRORE: La password deve essere minore di 64 caratteri.\n");
 		return EXIT_FAILURE;
 	}
+	
 	/* Stampo a video le informazioni immesse */
-	fprintf(stdout, "Host: %s\nUser: %s\nPass: %s\n", host,user,pass); /* trovare metodo più sicuro per password */
+	fprintf(stdout, "Host: %s\nUser: %s\nPass: %s\n", host,user,pass);
 
 	/* Azzero il buffer della password e libero la memoria occupata */
-	memset(pass,0,strlen(pass));
+	memset(pass,0,(strlen(pass)+1));
 	free(host);
 	free(user);
 	free(pass);
