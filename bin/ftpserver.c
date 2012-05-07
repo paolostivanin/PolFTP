a4a48b432d4008d4b7471019acaad03847973267
Paolo Stivanin <admin@polslinux.it>
2012-05-07 16:50:19 +0200

    developing server...

--- bin/ftpserver.c
+++ bin/ftpserver.c
@@ -15,6 +15,39 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <sys/fcntl.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <net/if.h>  
+
+
+#define BUFFERSIZE 50
+#define PORT 21
+#define LINK_NUM 2
+
+#define USER 0
+#define PASS 1
+#define SYST 2
+#define TYPE 3
+#define PWD  4
+#define CWD  5
+#define PASV 6
+#define PORTN 7
+#define LIST 8
+#define RETR 9
+#define STOR 10
+#define QUIT 11
+#define DELE 12
+#define MKD 13
+#define UNKOWN 13
+
+#define CMDNOTFOUNT -1
+#define CMDNUM 14
+#define WLAN_NAME "wlan0"
+
+const char* commands[] = {"USER","PASS","SYST","TYPE","PWD","CWD","PASV","PORT","LIST","RETR","STOR","QUIT","DELE","MKD"};
 
 int main(){
 }
