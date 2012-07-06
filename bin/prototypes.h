64381d36bdde60c2b024434b70c981baff1c9db6
Paolo Stivanin <admin@polslinux.it>
2012-07-06 17:01:36 +0200

    updated prototypes.h

--- bin/prototypes.h
+++ bin/prototypes.h
@@ -3,7 +3,6 @@
 #include <stdint.h>
 void check_before_start(int, char **);
 void sig_handler(int, int, int, int);
-extern void clear_buf(char *, char *, char *, int);
 extern uint32_t file_list(char *, char ***);
 extern void onexit(int, int, int, int);
 #endif
